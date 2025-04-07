ad28496e300b4a5208b6667a1a3c3be93f9103ba
m-triple-m <triplem656@gmail.com>
2025-04-08 00:24:26 +0530

    fix statcard

--- src/components/statCard/index.tsx
+++ src/components/statCard/index.tsx
@@ -7,12 +7,12 @@ interface StatCardProps {
     value: number;
     label?: string;
     description?: string;
-    color?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | undefined;
+    color?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info' | 'dark' | 'light' | undefined;
 }
 
 const StatCard: React.FC<StatCardProps> = ({ value, label, description, color = 'primary' }) => {
     return (
-        <div className={clsx('p-8 rounded-xl', tailwindThemeClasses.colors[color])}>
+        <div className={clsx('p-8 rounded-xl', color === 'light' ? 'text-black border' : 'text-white' , tailwindThemeClasses.backgroundColor[color])}>
             <h1 className='text-5xl font-bold mb-3'>{value}</h1>
             <p className='text-md uppercase font-bold mb-1'>{label}</p>
             <p className='text-sm'>{description}</p>
